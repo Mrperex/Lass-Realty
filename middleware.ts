ec88268c496caa50e8e8d881f09576c7ca024471
Pablo Arturo Perez Brito <mrperex@Pablos-Mac-mini.local>
2026-03-17 00:00:19 -0400

    Fix Next-intl middleware intercepting SVGs causing redirect loops

--- middleware.ts
+++ middleware.ts
@@ -98,6 +98,6 @@ export const config = {
     // - _next/static (static files)
     // - _next/image (image optimization files)
     // - favicon.ico (favicon file)
-    // - extensions (.xml, .txt, .js, .json)
-    matcher: ['/((?!api|_vercel|_next/static|_next/image|favicon.ico|.*\\.(?:xml|txt|js|json|png|jpg|jpeg|gif|webp)$).*)'],
+    // - extensions (.xml, .txt, .js, .json, images)
+    matcher: ['/((?!api|_vercel|_next/static|_next/image|favicon.ico|.*\\.(?:xml|txt|js|json|png|jpg|jpeg|gif|webp|svg|ico)$).*)'],
 }
