52d9d0e049273fb0e1f593c1e09d56c4b565b587
Pablo Arturo Perez Brito <mrperex@Pablos-Mac-mini.local>
2026-03-22 23:53:37 -0400

    Fix syntax error in PropertyGallery
    - Remove extra semicolon causing build failure
    - Fix malformed return statement in map function

--- components/PropertyGallery.tsx
+++ components/PropertyGallery.tsx
@@ -190,7 +190,7 @@ export default function PropertyGallery({ images, videos, title }: {
                             </>
                         )}
                         </div>
-                        );
+                    );
                     })}
 
                 {/* Supporting Items */}
